aff0007bf646d014b5f826c1db77657ca07b4723
Chence Shi <ascc594557067@gmail.com>
2023-04-17 02:07:47 -0700

    changed new postgres connection,updated migration.

--- prisma/migrations/20230417090336_just_a_post/migration.sql
+++ prisma/migrations/20230417090336_just_a_post/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
 CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
+    "title" TEXT NOT NULL,
     "content" TEXT,
     "published" BOOLEAN NOT NULL DEFAULT false,
 
--- prisma/migrations/20230417090456_we_have_user_and_post_model/migration.sql
+++ prisma/migrations/20230417090456_we_have_user_and_post_model/migration.sql
@@ -1,18 +1,11 @@
 /*
   Warnings:
 
-  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - Added the required column `userId` to the `Post` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
-ADD COLUMN     "userId" TEXT NOT NULL,
-ALTER COLUMN "id" DROP DEFAULT,
-ALTER COLUMN "id" SET DATA TYPE TEXT,
-ALTER COLUMN "title" SET DATA TYPE TEXT,
-ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
-DROP SEQUENCE "Post_id_seq";
+ALTER TABLE "Post" ADD COLUMN     "userId" TEXT NOT NULL;
 
 -- CreateTable
 CREATE TABLE "Account" (
